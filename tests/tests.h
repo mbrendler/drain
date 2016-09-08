ab2d5919dd688392752a16ab50c3f244b3bd4861
Maik Brendler <maik.brendler@injixo.com>
2016-09-08 20:28:38 +0200

    Add ASSERT and ASSERT_PONTER macros

--- tests/tests.h
+++ tests/tests.h
@@ -4,24 +4,36 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <assert.h>
 
-#define ASSERT_STRING(expect, value) if (strcmp(expect, value)) { \
+#define ASSERT(x) if (!x) {                                                  \
+    printf("failure: %s:%d  %s\n", __FILE__, __LINE__, __PRETTY_FUNCTION__); \
+    exit(EXIT_FAILURE);                                                      \
+  }
+
+#define ASSERT_STRING(expect, value) if (strcmp(expect, value)) {          \
     printf("failure: %s:%d  %s\n    expected: '%s'\n         got: '%s'\n", \
-      __FILE__, __LINE__, __PRETTY_FUNCTION__, expect, value); \
-    exit(EXIT_FAILURE); \
+      __FILE__, __LINE__, __PRETTY_FUNCTION__, expect, value);             \
+    exit(EXIT_FAILURE);                                                    \
   }
 
-#define ASSERT_BYTES(size, expect, value) if (memcmp(expect, value, size)) { \
-    printf("failure: %s:%d  %s\n    expected: ", \
-      __FILE__, __LINE__, __PRETTY_FUNCTION__); \
+#define ASSERT_BYTES(size, expect, value) if (memcmp(expect, value, size)) {                          \
+    printf("failure: %s:%d  %s\n    expected: ",                                                      \
+      __FILE__, __LINE__, __PRETTY_FUNCTION__);                                                       \
     for (size_t i = 0; i < size ; ++i) { printf(isprint(expect[i]) ? " %c," : "%2.2x,", expect[i]); } \
-    printf("\n         got: "); \
-    for (size_t i = 0; i < size ; ++i) { printf(isprint(value[i]) ? " %c," : "%2.2x,", value[i]); } \
-    puts(""); \
+    printf("\n         got: ");                                                                       \
+    for (size_t i = 0; i < size ; ++i) { printf(isprint(value[i]) ? " %c," : "%2.2x,", value[i]); }   \
+    puts("");                                                                                         \
   }
 
-#define ASSERT_INT(expect, value) if (expect != value) { \
+#define ASSERT_INT(expect, value) if (expect != value) {               \
     printf("failure: %s:%d  %s\n    expected: %d\n         got: %d\n", \
-      __FILE__, __LINE__, __PRETTY_FUNCTION__, expect, value); \
-    exit(EXIT_FAILURE); \
+      __FILE__, __LINE__, __PRETTY_FUNCTION__, expect, value);         \
+    exit(EXIT_FAILURE);                                                \
+  }
+
+#define ASSERT_POINTER(expect, value) if (expect != value) {                 \
+    printf("failure: %s:%d  %s\n    expected: %p\n         got: %p\n",       \
+      __FILE__, __LINE__, __PRETTY_FUNCTION__, (void*)expect, (void*)value); \
+    exit(EXIT_FAILURE);                                                      \
   }
