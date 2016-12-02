1796fc58f2f1536a539ff54e6272d98f02c2910b
Maik Brendler <maik.brendler@injixo.com>
2016-12-02 15:31:32 +0100

    Add drainfile does not exist test

--- tests/test_drainfile.c
+++ tests/test_drainfile.c
@@ -7,6 +7,7 @@ void test_drainfile_read() {
     asprintf(&filename, "%s/drainfile", dirname(__FILE__));
     ProcessList *l = drainfile_read(filename);
     free(filename);
+    filename = NULL;
     ASSERT(l);
     ASSERT_STRING("proc-1", l->p.name);
     ASSERT_STRING("echo proc-1", l->p.cmd);
@@ -18,6 +19,16 @@ void test_drainfile_read() {
     process_list_free(l);
 }
 
+void test_drainfile_read_file_not_exist() {
+    char *filename = NULL;
+    asprintf(&filename, "%s/not-existing-drainfile", dirname(__FILE__));
+    ProcessList *l = drainfile_read(filename);
+    free(filename);
+    filename = NULL;
+    ASSERT(!l)
+}
+
 int main() {
     test_drainfile_read();
+    test_drainfile_read_file_not_exist();
 }
