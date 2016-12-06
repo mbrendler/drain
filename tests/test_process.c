5ca4a16c2900e004502e9ee9d0aba3123264b7d1
Maik Brendler <maik.brendler@injixo.com>
2016-12-06 11:23:16 +0100

    Add test for process_clear

--- tests/test_process.c
+++ tests/test_process.c
@@ -15,7 +15,21 @@ void test_init() {
     ASSERT_STRING("a-process-command", p.cmd);
 }
 
-// void process_clear(Process *p);
+void test_clear() {
+    Process p;
+    process_init(&p, "a-process-name", "a-process-command", 23, 42);
+    process_clear(&p);
+
+    ASSERT_INT(23, p.color);
+    ASSERT_INT(-1, p.pid);
+    ASSERT_INT(42, p.fd);
+    ASSERT(!p.f);
+    ASSERT_INT(0, p.out_fd_count);
+    ASSERT(!p.out_fds);
+    ASSERT(!p.name);
+    ASSERT(!p.cmd);
+}
+
 // void process_start(Process *p);
 // int process_forward(Process *p);
 // void process_stop(Process *p);
@@ -27,4 +41,5 @@ void test_init() {
 
 int main() {
     test_init();
+    test_clear();
 }
