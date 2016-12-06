d86629cae20d665cb196fa1d12d03d0b88e5b956
Maik Brendler <maik.brendler@injixo.com>
2016-12-06 12:45:51 +0100

    Add test for process_add_output_fd

--- tests/test_process.c
+++ tests/test_process.c
@@ -33,7 +33,21 @@ void test_clear() {
 // void process_start(Process *p);
 // int process_forward(Process *p);
 // void process_stop(Process *p);
-// void process_add_output_fd(Process *p, int fd);
+
+void test_add_output_fd() {
+    Process p;
+    process_init(&p, "a-process-name", "a-process-command", 23, 42);
+
+    process_add_output_fd(&p, 394);
+    ASSERT_INT(1, p.out_fd_count);
+    ASSERT_INT(394, *p.out_fds);
+
+    process_add_output_fd(&p, 123);
+    ASSERT_INT(2, p.out_fd_count);
+    ASSERT_INT(394, *p.out_fds);
+    ASSERT_INT(123, *(p.out_fds + 1));
+}
+
 // void process_remove_output_fd(Process *p, int index);
 // int process_print_status(const Process* p);
 // int process_serialize(Process *p, char* buffer, int buf_size);
@@ -42,4 +56,5 @@ void test_clear() {
 int main() {
     test_init();
     test_clear();
+    test_add_output_fd();
 }
