9fbd8dd803725594ce38743dfa39a7710dc0a648
Maik Brendler <maik.brendler@injixo.com>
2017-07-21 15:48:01 +0200

    Add -j option with CPU-count to make calls

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,14 +2,16 @@
 
 set -xe
 
+CPU_COUNT="$( (sysctl -n hw.ncpu || grep -c '^processor *' /proc/cpuinfo || echo 1) 2>/dev/null)"
+
 rm -rf BD_release BD_debug
 mkdir BD_release BD_debug
 
 cd BD_release
 cmake -DCMAKE_BUILD_TYPE=Release ..
-make all test
+make -j"$CPU_COUNT" all test
 cd ..
 
 cd BD_debug
 cmake -DCMAKE_BUILD_TYPE=Debug ..
-make all test
+make -j"$CPU_COUNT" all test
